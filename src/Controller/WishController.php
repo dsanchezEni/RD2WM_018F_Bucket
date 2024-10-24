10346876207a511db93d296719ca506bd877d572
dsanchez <dsanchez@campus-eni.fr>
2024-10-24 09:57:08 +0200

    Module 05 TP1 - Gestion du controller et de ses routes

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -6,10 +6,10 @@ use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+#[Route('/wishes', name: 'app_wish_')]
 class WishController extends AbstractController
 {
-    #[Route('/wishes', name: 'app_wish_list',methods: ['GET'])]
+    #[Route('/', name: 'list',methods: ['GET'])]
     public function list(WishRepository $wishRepository): Response
     {
         //Récupère les wishs qui sont publiés, et trie les du plus récent au plus ancien
@@ -20,7 +20,7 @@ class WishController extends AbstractController
         ]);
     }
 
-    #[Route('/wishes/{id}', name: 'app_wish_detail',requirements:['id'=>'\d+'],methods: ['GET'])]
+    #[Route('/{id}', name: 'detail',requirements:['id'=>'\d+'],methods: ['GET'])]
     public function detail(int $id,WishRepository $wishRepository): Response
     {
         //Récupère le wish en fonction de l'id présent dans l'URL
